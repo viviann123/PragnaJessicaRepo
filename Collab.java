fb8d391dcdbf7e1e9961a9ad4096d1c4205ea495
jessical32438 <jessica083000@gmail.com>
2025-11-14 14:04:26 -0500

    Part2

--- Collab.java
+++ Collab.java
@@ -1,8 +1,17 @@
 public class Collab{
-	public int[] createArray(int n){
+	public int[] Part1(int n){
 		int[] arr = new int[n];
 		for(int i = 0; i<n; i++)
 			arr[i] = (int)(Math.random()*10)+1;
 		return arr;
+	}
+	public int Part2(int[][] arr){
+		for(int i = 0; i<arr.length; i++){
+			for(int j = 0; j< arr[0].length; j++){
+				if(arr[i][j]%2==0)
+					return arr[i][j];
+			}
+		}
+		return 0;
 	}
 }
