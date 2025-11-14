1b464096e7f5283246b89cf02189fd5d1bd7dc0d
jessical32438 <jessica083000@gmail.com>
2025-11-14 14:12:33 -0500

    Part2v2

--- Collab.java
+++ Collab.java
@@ -8,8 +8,19 @@ public class Collab{
 	public int Part2(int[][] arr){
 		for(int i = 0; i<arr.length; i++){
 			for(int j = 0; j< arr[0].length; j++){
-				if(arr[i][j]%2==0)
-					return arr[i][j];
+				if(arr[i][j]%2==0){
+					int sum = 0;
+					if(j>0)
+						sum+=arr[i][j-1];
+					if(i>0)
+						sum+=arr[i-1][j];
+					if(i<arr.length)
+						sum+=arr[i+1][j];
+					if(j<arr.length)
+						sum+=arr[i][j+1];
+					if(sum>17)
+						return sum;
+				}
 			}
 		}
 		return 0;
